b513fff5de6fc6e02c1238b88cb663651e66c234
Mariano Dubois <71388836+MarianoDubois@users.noreply.github.com>
2025-03-20 15:07:41 -0300

    bound.c guia 0 hecho

--- clases/Algoritmos y Estructuras de Datos 2/bounds.c
+++ clases/Algoritmos y Estructuras de Datos 2/bounds.c
@@ -1,26 +1,8 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include <assert.h>
 
-
-
-void set_array(int n_max, int A[]){
-    int i=0;
-    printf("introduzca los valores de su array:\n");
-    while (i < n_max){
-        scanf("%d", &A[i]);
-        printf("valor del array en A[%d] = %d\n", i, A[i]);
-        i++;
-    }
-}
-
-void print_array(int n_max, int A[]){
-    int i = 0;
-    while(i < n_max){
-        printf("lugar:[%d] valor: %d\n", i, A[i]);
-        i++;
-    }
-}
+#define ARRAY_SIZE 4
 
 struct bound_data {
     bool is_upperbound;
@@ -29,43 +11,68 @@ struct bound_data {
     unsigned int where;
 };
 
-struct bound_data check_bound(int value, int arr[], unsigned int length){
-    struct bound_data temp_data;
-    temp_data.is_lowerbound = 1;
-    temp_data.is_upperbound = 1;
-    temp_data.exists = 0;
-    for (int i = 1; i <= length; i++){
-        if (temp_data.is_upperbound == 1 && temp_data.is_lowerbound == 1 && temp_data.exists == 0){
-            if (value == arr[i]){
-                temp_data.exists = 1;
-                temp_data.where = i;
-            }else if(value > arr[i]){
-                
-            }
+struct bound_data check_bound(int value, int arr[], unsigned int length) {
+    struct bound_data res;
+    res.is_lowerbound = 0;
+    res.is_upperbound = 0;
+    res.exists = 0;
+    res.where = -1;
+    int min = arr[0];
+    int max = arr[0];
+    for (unsigned int i = 0; i < length; i++){
+        if(max < arr[i]){
+            max = arr[i];
+        }
+        if(min > arr[i]){
+            min = arr[i];
+        }    
+        if(value == arr[i]){
+            res.where = i+1;
+            res.exists = 1;
         }
     }
-}
-
-void Lab0(void){
-    int length;
-    printf("introduzca el tamaño de su array:\n");
-    scanf("%d",&length);
-    int arr[length];
-    set_array(length,arr);
-    print_array(length,arr);
-    struct bound_data check_bound(int value, int arr[], unsigned int length);
-
-}
-
-int main(void){
-    int condition;
-    printf("cual Lab queres testear? \n");
-    scanf("%d",&condition);
-    switch(condition){
-        case 1:
-            lab_0();
-            break;
+    if(value >= max){
+        res.is_upperbound = 1;
+    }else if(value <= min){
+        res.is_lowerbound = 1;
     }
-
-    return 0;
+    return res;
 }
+
+int main(void) {
+    int a[ARRAY_SIZE] = {0, -1, 9, 4};
+    int value=9;
+    int i=0;
+    printf("introduzca los valores de su array:\n");
+    while (i < ARRAY_SIZE){
+        scanf("%d", &a[i]);
+        printf("valor del array en A[%d] = %d\n", i, a[i]);
+        i++;
+    }
+    printf("introduzca el valor que desea checkear de su array:\n");
+    scanf("%d", &value);
+    struct bound_data result = check_bound(value, a, ARRAY_SIZE);
+    
+    printf("\n%d\n%d\n%d\n%u\n", result.is_upperbound, result.is_lowerbound, result.exists, result.where);
+    
+    printf("\nsu resultado es:\n");
+
+    if(result.is_upperbound == 1){
+        if(result.exists == 1 && a[result.where-1] == value){
+            printf("maximo en la posicion %u\n", result.where);
+        }else{
+            printf("cota superior\n");
+        }
+    }else if(result.is_lowerbound == 1){
+        if(result.exists == 1 && a[result.where-1] == value){
+            printf("minimo en la posicion %u\n", result.where);
+        }else{
+            printf("cota inferior\n");
+        }
+    }else{
+        printf("no es nada\n");
+    }             
+
+    return EXIT_SUCCESS;
+}
+
