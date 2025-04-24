f8d75b7df83b7e6c2541edbbdb629f3ba81f63c6
Mariano Dubois <nariuk95@gmail.com>
2025-04-24 00:29:19 -0300

    lab02 ej3 done

--- clases/Algoritmos y Estructuras de Datos 2/Lab02/ej3/cima_log.c
+++ clases/Algoritmos y Estructuras de Datos 2/Lab02/ej3/cima_log.c
@@ -15,9 +15,49 @@
  * @param a Arreglo.
  * @param length Largo del arreglo.
  */
-int cima_log(int a[], int length) {
+int cima_rec(int a[], int left, int right)
+{
+    //variables
+    int cima_pos = -1;
+    int middle = (left+right)/2;
 
-    // COMPLETAR!!
+    //printing array & variables
+    printf("\nlos valores de las variables al inicializar son:\nleft=%d | right=%d | cima_pos=%d | middle=%d\n",left,right,cima_pos,middle);
+    for (int i = left; i < right; i++)
+    {
+        printf("%d,",a[i]);
+    }
+    printf("%d\n",a[right]);
 
-    return 0;
+    //codigo/logica
+    if (left == right)//""caso base""
+    {
+        cima_pos = left;
+        printf("llegamos a un caso particular :/ donde cima_pos vale siempre middle == %d\n",cima_pos);
+    }
+    else if (a[middle] > a[middle+1] && a[middle] > a[middle-1])//check de cima
+    {
+        cima_pos = middle;
+        printf("encontramos la posicion de la cima, cima_pos = %d\n",cima_pos);
+    }
+    else if (a[middle] < a[middle+1])//recursion por derecha del array
+    {
+        cima_pos = cima_rec(a,middle+1,right);
+        printf("hacemos recursion en la derecha de la particion\n");
+    }
+    else//recursion por izquierda
+    {
+        cima_pos = cima_rec(a,left,middle-1);
+        printf("hacemos recursion en la izquierda de la particion\n");
+    }
+
+    //output
+    printf("los valores de las variables al finalizar son:\nleft=%d | right=%d | cima_pos=%d | middle=%d\n\n",left,right,cima_pos,middle);
+    return cima_pos;
 }
+
+int cima_log(int a[], int length) {
+    int cima = cima_rec(a,0,length-1);
+    return cima;
+}
+
--- clases/Algoritmos y Estructuras de Datos 2/Lab02/ej3/tests.c
+++ clases/Algoritmos y Estructuras de Datos 2/Lab02/ej3/tests.c
@@ -3,7 +3,7 @@
 #include "cima_log.h"
 
 #define MAX_LENGTH 10
-#define N_TESTCASES_CIMA 1
+#define N_TESTCASES_CIMA 10
 
 int main() {
     struct testcase {
@@ -14,9 +14,15 @@ int main() {
 
     struct testcase tests[N_TESTCASES_CIMA] = {
         { {8}, 1, 0 },
-
-        // COMPLETAR!!
-
+        { {1, 2, 3, 2, 1}, 5, 2 },                 // cima en medio
+        { {5, 4, 3, 2, 1}, 5, 0 },                 // cima en 0
+        { {1, 2, 3, 4, 5}, 5, 4 },                 // cima en n−1
+        { {1, 3, 2}, 3, 1 },                       // cima en el medio exacto
+        { {1, 5, 3, 2, 0}, 5, 1 },                 // cima al principio pero no en 0
+        { {3, 7, 6}, 3, 1 },                       // cima al medio en arreglo corto
+        { {-10, -5, 0, -1, -4}, 5, 2 },            // incluye negativos, cima en 2
+        { {10, 20, 30, 40, 35, 25, 10}, 7, 3 },    // cima en el medio en arreglo largo
+        { {3, 6, 9, 12, 15, 13, 10, 8, 5, 1}, 10, 4 }
     };
     int result;
 
